3a2371368d9688ae2ba90a6ad8743d9753c10faf
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-12-30 09:54:30 -0500

    update pin create view

--- frontend/src/pages/PinCreate/index.jsx
+++ frontend/src/pages/PinCreate/index.jsx
@@ -6,12 +6,12 @@ import { AiOutlineCloudUpload } from "react-icons/ai";
 
 function UploadCard(){
     return(
-        <div className="px-4 outline-dashed outline-gray-400 h-full py-16">
+        <div className="px-4 outline-dashed outline-gray-400 h-full py-12 md:py-16">
             <div className="flex flex-col items-center">
                 <AiOutlineCloudUpload size={30}/>
                 <p>Click to Upload</p>
             </div>
-            <p className="text-gray-400 mt-24 text-justify text-sm">
+            <p className="text-gray-400 mt-12 md:mt-24 text-justify text-sm">
                 Recommendation: Use high-quality JPG, JPEG, SVG, PNG, GIF or TIFF less than 20MB
             </p>
         </div>
@@ -29,7 +29,7 @@ export function PinCreate() {
         <section className="min-h-screen">
             <div className="flex flex-col md:flex-row max-w-[900px] md:mt-12 overflow-hidden bg-white mx-auto px-4 py-4">
                 <div className="w-full md:w-[45%]">
-                    <div className="h-[400px] bg-[#F0F0F0] p-4">
+                    <div className="h-[300px] md:h-[400px] bg-[#F0F0F0] p-4">
                         <UploadCard/>
                     </div>
                 </div>    
